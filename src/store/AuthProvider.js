82135f58e32d2fec54359dd62bdb540aa53474cf
John Law <johnlaw114@gmail.com>
2023-02-12 22:21:50 -0500

    added authprovider

---
+++ src/store/AuthProvider.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import AuthContext from './auth-context.js';
+
+export const AuthConextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserLoggin = localStorage.getItem('isLoggedIn');
+
+    if (storedUserLoggin === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  //add use reducer like cart provider in food app
+
+  const loginHandler = () => {
+    localStorage.setItem('isLoggedIn', '1');
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.setItem('isLogged', '0');
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,42 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 const AuthContext = React.createContext({
   isLoggedIn: false,
   onLogout: () => {},
   onLogin: (email, password) => {},
 });
-
-export const AuthConextProvider = (props) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    const storedUserLoggin = localStorage.getItem('isLoggedIn');
-
-    if (storedUserLoggin === '1') {
-      setIsLoggedIn(true);
-    }
-  }, []);
-
-  const loginHandler = () => {
-    localStorage.setItem('isLoggedIn', '1');
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    localStorage.setItem('isLogged', '0');
-    setIsLoggedIn(false);
-  };
-
-  return (
-    <AuthContext.Provider
-      value={{
-        isLoggedIn: isLoggedIn,
-        onLogout: logoutHandler,
-        onLogin: loginHandler,
-      }}>
-      {props.children}
-    </AuthContext.Provider>
-  );
-};
 
 export default AuthContext;
