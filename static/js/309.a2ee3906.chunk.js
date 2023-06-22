ec49fb1035399af14c2e14b217102c3d70a55b88
John Law <johnlaw114@gmail.com>
2023-06-22 02:35:41 -0400

    Updates

---
+++ static/js/309.a2ee3906.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkthe_portfolio=self.webpackChunkthe_portfolio||[]).push([[309],{7309:function(e,t,o){o.r(t);o(2791);var r=o(184);t.default=function(){return(0,r.jsx)("h2",{children:"Registration Successful! Welcome to The Party!"})}}}]);
+//# sourceMappingURL=309.a2ee3906.chunk.js.map
